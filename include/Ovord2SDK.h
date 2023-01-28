b160f03873ca369e06f3970b72787773cab6d6a1
jerensl <jerenslensun@gmail.com>
2023-01-28 11:19:26 +0800

    fix the broken build

--- include/Ovord2SDK.h
+++ include/Ovord2SDK.h
@@ -1,16 +1,19 @@
 #ifndef Ovord2SDK_h
 #define Ovord2SDK_h
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 class Ovord2SDK {
     private:
-        char *shortName;
-        char *token;
-        char *rootCACertificate;
+        const char *shortName;
+        const char *token;
+        const char *rootCACertificate;
     
     public: 
         Ovord2SDK(char *shortName, char *token);
-        void Ovord2SDK::sendData(String sensors[], float values[]);
+        void sendData(String sensors[], float values[]);
 };
 
 #endif
